69b5a4780aa892014f0e5b3d62b608f311da5cfb
Yaroslav Ganshin <139777613+yarky628@users.noreply.github.com>
2025-12-13 16:40:16 +0300

    Update config.py
    Да, забыл добавить для редиса, еще проверял health и думал, почему редис не откликается, теперь добавил в конфиг

--- app/config.py
+++ app/config.py
@@ -1,5 +1,9 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv() 
 
 FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:5173")
 GITHUB_CLIENT_ID = os.getenv("GITHUB_CLIENT_ID")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0") # Добавляем REDIS_URL
